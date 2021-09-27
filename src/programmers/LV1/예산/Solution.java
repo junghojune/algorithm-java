db3d80754dae65f30b5fb083750c0190354ead1c
Hojune Jung <hojune0904@gmail.com>
2021-09-27 10:43:59 +0900

    숫자문자열과 영단어

---
+++ src/programmers/LV1/예산/Solution.java
@@ -0,0 +1,22 @@
+package programmers.LV1.예산;
+
+public class Solution {
+
+    // 신청한 부서에는 정확한 금액 지원, 낮은 금애긍ㄴ 지원 불가능
+    // 최대 지원 가능한 수 return
+
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String str = "onetwothree";
+        String target = "two";
+        int targer_num = str.indexOf(target);
+        System.out.println(str.substring(targer_num,targer_num+3));
+
+    }
+}
---
+++ src/programmers/kakao/LV1/숫자문자열과영단어/Solution.java
@@ -0,0 +1,67 @@
+package programmers.kakao.LV1.숫자문자열과영단어;
+
+public class Solution {
+
+    public static int solution(String s) {
+        int answer = change(s);
+        return answer;
+    }
+
+    public static int change(String s) {
+
+
+        if(s.contains("one")) {
+
+            s = s.replaceAll("one", "1");
+        }
+
+        if(s.contains("two")) {
+
+            s = s.replaceAll("two", "2");
+        }
+
+        if(s.contains("three")) {
+
+            s = s.replaceAll("three", "3");
+        }
+        if(s.contains("four")) {
+
+            s = s.replaceAll("four", "4");
+        }
+        if(s.contains("five")) {
+
+            s = s.replaceAll("five", "5");
+        }
+        if(s.contains("six")) {
+
+            s = s.replaceAll("six", "6");
+        }
+        if(s.contains("seven")) {
+
+            s = s.replaceAll("seven", "7");
+        }
+        if(s.contains("eight")) {
+
+            s = s.replaceAll("eight", "8");
+        }
+
+        if(s.contains("nine")) {
+
+            s = s.replaceAll("nine", "9");
+        }
+
+
+
+        int answer = Integer.parseInt(s);
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        String str = "2three45sixsixsixseven";
+
+        System.out.println(solution(str));
+    }
+}
