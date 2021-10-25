6052d28c2302a3876a43bd0865810813239ea2ad
Hojune Jung <hojune0904@gmail.com>
2021-10-25 16:56:09 +0900

    노드삭제

--- src/studyDataStructure/linkedList/SingleLinkedList.java
+++ src/studyDataStructure/linkedList/SingleLinkedList.java
@@ -67,17 +67,39 @@ public class SingleLinkedList<T> {
 
         }
     }
+
+    public boolean delNode(T isData){
+        if(this.head == null){
+            return false;
+        }
+        else {
+            Node<T> node =  this.head;
+            if(node.data == isData){
+                this.head = this.head.next;
+                return true;
+            }else {
+                while (node.next != null) {
+                    if(node.next.data == isData){
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
+        }
+
+    }
     public static void main(String[] args) {
         SingleLinkedList<Integer> myLinkedList = new SingleLinkedList<Integer>();
         myLinkedList.addNode(1);
         myLinkedList.addNode(2);
         myLinkedList.addNode(3);
         myLinkedList.addNode(4);
-
-        System.out.println(myLinkedList.head.data);
-        System.out.println(myLinkedList.head.next.data);
+        myLinkedList.addNode(5);
 
         myLinkedList.addNodeInside(5,1);
+        myLinkedList.delNode(20);
         myLinkedList.printAll();
 
 
